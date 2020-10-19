2c5a376950d400c8559f31d7432a0606247bcc61
Agoston Reguly <regulyagoston@gmail.com>
2020-10-19 15:38:15 +0200

    class4_after

--- Class_4/codes/billion_price_project.R
+++ Class_4/codes/billion_price_project.R
@@ -12,7 +12,7 @@ rm(list=ls())
 library(tidyverse)
 library(moments)
 # new package we use
-#install.packages("readxl")
+# install.packages("readxl")
 library(readxl)
 
 ## Import data
@@ -130,7 +130,8 @@ ggplot( data = bpp ) +
 ggplot( data = bpp ) +
   geom_density( aes( x = p_diff ) , color = 'blue'  , alpha = 0.1 ) +
   labs(x = "Price differences",
-       y = "Relative Frequency" )
+       y = "Relative Frequency" ) +
+  xlim(-4,4)
 
 # Check for price differences
 chck <- bpp %>% filter( p_diff > 500 | p_diff < -500 )
@@ -138,5 +139,71 @@ chck <- bpp %>% filter( p_diff > 500 | p_diff < -500 )
 bpp <- bpp %>% filter( p_diff < 500 & p_diff > -500 )
 rm( chck )
 
+######
+## Creating factors in R
+# tell R that they are nominal qualitative data
+bpp$country <- factor( bpp$COUNTRY )
+table(bpp$country)
+
+# Calculate the mean for each country
+bpp %>% select( country , p_diff ) %>% 
+  group_by( country ) %>% 
+  summarize( mean = mean( p_diff) ,
+             sd = sd( p_diff ) , 
+             num_obs = n() )
+
+# Create ggplot for countries: histogram
+ggplot(data = bpp , aes( x = p_diff , fill = country ) ) +
+  geom_histogram( aes( y = ..density.. ), alpha =0.4 ) +
+  labs( x = "Price" , y = 'Relative Frequency' ,
+       fill = 'Country' ) +
+  facet_wrap(~country)+
+  xlim(-4,4)
+
+# HW: do the same with impute variable:
+#   1) recode it as a string
+#       a) if NA -> 'yes'
+#       b) if 1 -> 'no'
+#   2) Call factor variable as 'same_day'
+#   3) Create a summary by countries and by same_day
+
+
+######
+# HYPOTHESIS TESTING
+
+# Test: H0: the average price difference 
+#             between price_online - price = 0
+#       HA: the avg price diff is non 0.
+
+t.test( bpp$p_diff , mu = 0 )
+
+# Test 2: The online prices are smaller or equal to offline prices
+#   H0: price_online - price <= 0
+#   HA: price_online - price >  0
+t.test( bpp$p_diff , mu = 0 , alternative = "greater" )
+
+# Test 3: The online prices are larger or equal to offline prices
+#   H0: price_online - price >= 0
+#   HA: price_online - price <  0
+t.test( bpp$p_diff , mu = 0 , alternative = "less" )
+
+## HW: Filter to USA and price < 1000
+# two-sided t-test
+
+# Multiple hypothesis testing
+testing <- bpp %>% 
+          select( country , p_diff ) %>% 
+          group_by( country ) %>% 
+          summarize( mean_pdiff = mean( p_diff ) ,
+                     se_pdiff = 1/sqrt(n())*sd(p_diff),
+                     num_obs = n() )
+
+testing
+testing <- mutate( testing , t_stat = mean_pdiff / se_pdiff )
+testing
+testing <- mutate( testing , p_val = pt( -abs( t_stat ) , df = num_obs - 1 ) )
+testing
+testing <- mutate( testing ,  p_val = round( p_val , digit = 4 ) )
+testing
 
 
